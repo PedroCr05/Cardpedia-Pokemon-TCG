3fd0554461ad3970a3d13795b20bc83f1700ffc6
pedrocruz <JayJMarquez05@gmail.com>
2024-09-29 13:28:22 -0500

    Adding downloaded fonts into CSS

--- Minigames-code/Higher_or_Lower_Main/Higher-or-Lower.js
+++ Minigames-code/Higher_or_Lower_Main/Higher-or-Lower.js
@@ -1,8 +1,5 @@
-const cardData = document.querySelector('#pkmn-card');
-
-
-searchButton.addEventListener('click', async () => {
-  const pokemonName = pokemonNameInput.value;
-  const response = await axios.get(`https://api.tcgdex.net/v2/en/cards/${pokemonName}`);
-  console.log(response.data);
-});
+// searchButton.addEventListener('click', async () => {
+//   const pokemonName = pokemonNameInput.value;
+//   const response = await axios.get(`https://api.tcgdex.net/v2/en/cards/${pokemonName}`);
+//   console.log(response.data);
+// });
