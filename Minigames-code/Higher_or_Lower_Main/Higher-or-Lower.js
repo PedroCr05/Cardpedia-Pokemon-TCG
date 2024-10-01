ff7e721625af17b7599762409e2be43677e09811
pedrocruz <JayJMarquez05@gmail.com>
2024-10-01 18:53:47 -0500

    Swapped some line from HTML and added a few more descriptions for the grid in the center section in CSS. 2% NodeJS done

--- Minigames-code/Higher_or_Lower_Main/Higher-or-Lower.js
+++ Minigames-code/Higher_or_Lower_Main/Higher-or-Lower.js
@@ -1,1 +1,19 @@
-console.log(`NodeJS is currently working...`)
+/*---------------------------- Variables (state) ----------------------------*/
+
+let score = 0;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const higher = document.querySelector(`#up-arrow-outline`)
+const lower = document.querySelector(`#down-arrow-outline`)
+const 
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*----------------------------- Console Logging -----------------------------*/
