19b57b970e33996ff434874ce6912f6200429f7b
pedrocruz <JayJMarquez05@gmail.com>
2024-10-01 23:33:24 -0500

    Might swap API's or might do another idea... Will find out tomorrow. For now. Logging off for the night. Updated CSS, and JS...

--- Minigames-code/Higher_or_Lower_Main/Higher-or-Lower.js
+++ Minigames-code/Higher_or_Lower_Main/Higher-or-Lower.js
@@ -1,3 +1,7 @@
+// /*---------------------------- Global Variables ----------------------------*/
+
+const apiKey = `3415afd7-b9e7-47ec-bc98-0ad97808ac73`;
+
 /*---------------------------- Variables (state) ----------------------------*/
 
 let score = 0;
@@ -6,14 +10,32 @@ let score = 0;
 
 const higher = document.querySelector(`#up-arrow-outline`)
 const lower = document.querySelector(`#down-arrow-outline`)
-const 
+const timer = document.querySelector(`#time-display`);
+const messageDisplay = document.querySelector(`#message-display`);
+const plyrCard = document.querySelector(`#plyr-card`);
+const botCard = document.querySelector(`#bot-card`);
+const pkmnNames = document.querySelector(`.pkmn-names`);
 
 /*-------------------------------- Functions --------------------------------*/
 
-
+searchButton.addEventListener('click', async () => {
+  const pokemonName = pokemonNameInput.value;
+  const response = await axios.get(``);
+  console.log(response.data);
+});
 
 /*----------------------------- Event Listeners -----------------------------*/
 
-
-
 /*----------------------------- Console Logging -----------------------------*/
+
+
+
+// Step 1: fetch two random pokemon
+// step 2: user guess
+    // step 2.1: wrong
+        // step 2.1.1: Both cards wiped
+        // step 2.1.2: go back to step 1.
+    // step 2.2:
+        // step 2.2.1: Oppsing card is now player's card.
+        // step 2.2.2: Fetch 1 random opposing card.
+        // step 2.2.3: go back to step 2.
